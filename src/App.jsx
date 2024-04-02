a819b386927217511dcac3e30e4b62b199b7b9d4
nyarko-i <inyarko98@gmail.com>
2024-04-02 00:06:52 +0000

    staticPage

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import MainContent from "./MainContent"
+import './style.css'
+import Navbar from "./navbar"
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <MainContent/>
+    </div>
+  )
+}
+  
+export default App
---
+++ src/navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <header>
+      <nav className="nav">
+        <img src="./src/assets/logo.png" className="nav-logo" />
+        <h3>ReactFacts</h3>
+        <h4>React Course-Project 1</h4>
+      </nav>
+    </header>
+  );
+}
